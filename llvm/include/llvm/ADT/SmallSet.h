09a354280c76973b0fb5ed546f9566ca65b9e730
Victor Campos <victor.campos@arm.com>
2024-09-20 14:16:49 +0100

    fixup! [ADT] Add more useful methods to SmallSet API
    Remove inclusion of STLExtras.h

--- llvm/include/llvm/ADT/SmallSet.h
+++ llvm/include/llvm/ADT/SmallSet.h
@@ -14,7 +14,6 @@
 #ifndef LLVM_ADT_SMALLSET_H
 #define LLVM_ADT_SMALLSET_H
 
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/iterator.h"
